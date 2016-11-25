f47c83ffd37cea885cff6850d7d3ab1c9eb60ff8
MissDitch <mjtsloot@icloud.com>
2016-11-25 14:34:26 +0100

    improvements on validation; added 'previous'button: user can go back and change her answer; for questions that user has answered already, selected radio button is shown

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-var count = 0;
-var score = 0;
-var button = document.getElementById("btn");
+var index = 0;
+var formContainer = document.getElementById("formContainer");
 var form = document.form1;
+
+var prevButton = document.getElementById("prevButton");
+var nextButton = document.getElementById("nextButton");
+var scoreButton = document.getElementById("scoreButton");
+
+var storedAnswers = [];
 
 var allQuestions = [
   {
@@ -10,40 +15,78 @@ var allQuestions = [
     correctAnswer:0
   },
   {
-    question: "Waar komt de zon op?",
-    choices: ["noord", "west", "zuid", "oost"],
-    correctAnswer:3
+    question: "Who is Prime Minister of the Netherlands?",
+    choices: ["Diederik Samson", "Marc Rutte", "Geert Wilders", "Alexander Pechtold"],
+    correctAnswer:1
+  },
+  {
+    question: "In which city does the Dutch government reside?",
+    choices: ["Rotterdam", "Amsterdam", "The Hague", "Utrecht"],
+    correctAnswer:2
   }
 ];
 var quizLength = allQuestions.length;
 
-function nextQuestion() {
-  removeQuestion();
-
-  if (count >= quizLength) {
-    showScore();
+function showQuestion() {
+  if(index == 0) {
+    prevButton.style.display = "none";
+  }
+  if (index > 0) {
+    prevButton.style.display = "inline";
+  }
+  if(index == quizLength) {
+    scoreButton.style.display = "inline";
+    nextButton.style.display = "none";
+    form.innerHTML = "";
     return;
   }
+  else {
+    nextButton.style.display = "inline";
+    scoreButton.style.display = "none";
+  //  $("#nextButton").css("display", "inline");
+  //  $("#score").css("display", "none");
+  }
 
-  var choices = allQuestions[count].choices;
-  var quizItem = allQuestions[count];
+  var choices = allQuestions[index].choices;
+  var storedAnswer = storedAnswers[index];
+  var id;
+  var quizItem = allQuestions[index];
   var q = document.createElement("h3");
   var text = document.createTextNode(quizItem.question);
+
   q.appendChild(text);
   form.appendChild(q);
+
+  if (storedAnswer) {
+    id = storedAnswer.id;
+  }
 
   for (var i = 0; i < choices.length; i++) {
     var p = document.createElement("p");
     p.setAttribute("class", "answer");
 
     var input = document.createElement("input");
+    input.setAttribute("id", i);
     input.setAttribute("type", "radio");
     input.setAttribute("name", "question");
 
     if (i == quizItem.correctAnswer) {
-      input.setAttribute("value", "10");
+      input.setAttribute("value", "1");
     } else {
       input.setAttribute("value", "0");
+    }
+
+    if (i == id) {
+      input.setAttribute("checked", "checked");
+    }
+
+    input.onclick = function(e) {
+      var element = e.target;
+      var answer = {
+        id: element.id,
+        value: element.value
+      };
+      storedAnswers.push(answer);
     }
 
     var label = document.createElement("label");
@@ -56,42 +99,40 @@ function nextQuestion() {
   }
 }
 
-function removeQuestion() {
-  while (form.hasChildNodes()) {
-    form.removeChild(form.lastChild);
-  }
+function previousQuestion() {
+  form.innerHTML = "";
+  index--;
+  showQuestion(index);
 }
 
-function getAnswer() {
-  var value = 0;
-  var clicked = false;
-  var radios = document.form1.elements["question"];
-  for (var i = 0; i < radios.length; i++) {
-      if (radios[i].checked) {
-        clicked = true;
-        value = parseInt(radios[i].value);
-      }
+function nextQuestion() {
+  if (storedAnswers[index] == null) {
+    alert("Please choose an answer!");
+    return;
   }
-  if (clicked) {
-    count++;
-    score += value;
-    nextQuestion();
-  }
-  else {
-    alert("please answer!");
+  else  {
+    form.innerHTML = "";
+    index++;
+    showQuestion(index);
   }
 }
 
 function showScore() {
-  removeQuestion();
-  button.parentNode.removeChild(button);
+  prevButton.style.display = "none";
+  var totalScore = 0;
+  for (var i = 0; i < storedAnswers.length; i++) {
+    var score = parseInt(storedAnswers[i].value);
+    totalScore += score;
+  }
+
   var second = document.getElementById("second");
   var finalScore = document.getElementById("score");
   second.innerHTML = "Geweldig! Dit is het resultaat:";
-  finalScore.innerHTML = "Uw score is " + score;
+  finalScore.innerHTML = "Uw score is " + totalScore;
 }
 
+prevButton.addEventListener("click", previousQuestion);
+nextButton.addEventListener("click", nextQuestion);
+scoreButton.addEventListener("click", showScore);
 
-button.addEventListener("click", getAnswer);
-
-nextQuestion();
+showQuestion();
