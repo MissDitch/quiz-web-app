302b643d23c1a4e638cfc28058758703dfcf1643
MissDitch <mjtsloot@icloud.com>
2016-12-05 21:42:01 +0100

    quiz questions now stored in localStorage; init separate function; Player new attributes added; functions startQuiz and resumeQuiz added

--- js/main.js
+++ js/main.js
@@ -23,24 +23,66 @@ var passWarning = document.getElementById("passWarning");
 var loginBtn = document.getElementById("loginBtn");
 var accountBtn = document.getElementById("accountBtn");
 
-var quiz = document.getElementById("quiz");
+var actualPlayer;
+var playerString = localStorage.getItem("playerArray");
+var playerArray = JSON.parse(playerString);
+
+var message = document.getElementById("message");
+
+var startButton = document.getElementById("startButton");
+var quizContainer = document.getElementById("quizContainer");
+var quizNotify  = document.getElementById("quizNotify");
+var allQuestions;
+var quizLength;
+var choices;
+var storedAnswers;
+var index;
 var formContainer = document.getElementById("formContainer");
 var form = document.form1;
 var warning = document.getElementById("warning");
 
+var quizButtons = document.getElementById("quizButtons");
 var prevButton = document.getElementById("prevButton");
 var nextButton = document.getElementById("nextButton");
 var scoreButton = document.getElementById("scoreButton");
 
-var storedAnswers = [];
-var index = 0;
 
-quiz.style.display = "none";
-topBarRight.style.display = "none";
+quizContainer.setAttribute("style", "display:none");
+topBarRight.setAttribute("style", "display:none");
 
+//start.setAttribute("style", "display:none");
+quizButtons.setAttribute("style", "display:none");
 loginBtn.addEventListener("click", checkForm);
 accountBtn.addEventListener("click", checkForm);
 logout.addEventListener("click", logOut);
+startButton.addEventListener("click", startQuiz);
+resumeButton.addEventListener("click", resumeQuiz);
+
+// thanks to https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+// this function loads the quiz questions from external JSON file
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'http://localhost/quiz-web-app/js/quizQuestions.json', true);
+  //xobj.open('GET', 'file:///C:/Users/Marian standaard/Projecten/quiz-web-app/js/quizQuestions.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      /* Required use of an anonymous callback as .open will NOT return a value
+      but simply returns undefined in asynchronous mode  */
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+function init() {
+  loadJSON(function(response) {
+    // Parse JSON string into object
+    allQuestions = JSON.parse(response);
+    localStorage.setItem("allQuestions", response);
+  });
+}
+
 
 //checks if username and password are entered
 function checkForm(e) {
@@ -82,12 +124,16 @@ function Player(username, password) {
   }
   this.storedScores = [];
   this.visits = 0;
+  this.finished = true;
+  this.answerAt = 0;
+  this.storedAnswers = [];
 }
 
+Player.prototype.greet = function() {
+  console.log("Hi, I'm " + this.userName);
+}
+//Player.prototype.finished = false;
 
-var actualPlayer;
-var playerString = localStorage.getItem("playerArray");
-var playerArray = JSON.parse(playerString);
 
 function createAccount(username, password) {
   userWarning.innerHTML = "";
@@ -96,31 +142,59 @@ function createAccount(username, password) {
   player = new Player(username, password);
   playerArray.push(player);
   localStorage.setItem("playerArray", JSON.stringify(playerArray));
-  passWarning.innerHTML = "Account created, please enter your username and password";
+  accountNotify.innerHTML = "Account created, please enter your username and password";
   input1.value = "";
   input2.value = "";
 }
 
 function logIn(username, password) {
-  passWarning.innerHTML = "";
+//  passWarning.setAttribute("style", "display:none");
+//  accountNotify.setAttribute("style", "display:none");
+  createAccount.innerHTML = "";
   var playerString = localStorage.getItem("playerArray");
   playerArray = JSON.parse(playerString);
   console.log(playerArray);
 
   for (var i = 0; i < playerArray.length; i++) {
     var player = playerArray[i];
+  //  console.log(player.getUserName());
     if (player.userName === username && player.passWord === password) {
       console.log(player);
       actualPlayer = player;
+      storedAnswers = actualPlayer.storedAnswers;
+      index = actualPlayer.answerAt;
 
       login.setAttribute("style", "display:none");
-      topBarRight.setAttribute("style", "display:visible");
+      welcome.setAttribute("style", "display:none");
       //hides login panel and shows username and logout option
+      topBarRight.setAttribute("style", "display:visible");
       user.innerHTML = player.userName;
-      quiz.setAttribute("style", "display:visible");
-      //now welcome message and first question are shown
+      //shows welcome message and startbutton
+      quizContainer.setAttribute("style", "display:visible");
+      // three parts of quizContainer:
+      quizNotify.setAttribute("style", "display:visible");
+      formContainer.setAttribute("style", "display:none");
+      quizButtons.setAttribute("style", "display:none");
+
+      if (actualPlayer.visits === 0) {
+          message.innerHTML = "Welcome to this quiz, " + actualPlayer.userName + "! <br />Would you like to start?";
+          startButton.setAttribute("style", "display:visible");
+          resumeButton.setAttribute("style", "display:none");
+      }
+      else if (actualPlayer.visits > 0) {
+        if (actualPlayer.finished === false) {
+          message.innerHTML = "Welcome back " + actualPlayer.userName + "! <br />You want to finish the quiz?";
+          startButton.setAttribute("style", "display:none");
+          resumeButton.setAttribute("style", "display:visible");
+        }
+        else {
+          message.innerHTML = "Welcome back " + actualPlayer.userName + "!<br />Would you like to retake the quiz?";
+          startButton.setAttribute("style", "display:visible");
+          resumeButton.setAttribute("style", "display:none");
+        }
+      }
     }
-    else {
+    else if (player.userName != username || player.passWord != password){
       passWarning.innerHTML = "Username or password are not correct, <br>please try again";
       input1.value = "";
       input2.value = "";
@@ -129,22 +203,164 @@ function logIn(username, password) {
     input1.value = "";
     input2.value = "";
   }
-  if (actualPlayer.visits === 1) {
-    welcome.innerHTML = "Welcome to this quiz, " + actualPlayer.userName + "!";
-  }
-  else if (actualPlayer.visits > 1) {
-    welcome.innerHTML = "Welcome back to this quiz, " + actualPlayer.userName + "!";
-  }
 }
+
 
 function logOut(e) {
   e.preventDefault();
   login.setAttribute("style", "display:visible");
+  passWarning.setAttribute("style", "display:none");
   //shows login panel and hides username and logout option
   topBarRight.setAttribute("style", "display:none");
-  quiz.setAttribute("style", "display:none");
+  quizContainer.setAttribute("style", "display:none");
+  if (actualPlayer.finished){
+    actualPlayer.answerAt = 0;
+    actualPlayer.storedAnswers.length = 0;
+  }
+  else {
+    actualPlayer.answerAt = index;
+    actualPlayer.storedAnswers = storedAnswers;
+  }
+
   actualPlayer.visits += 1;
   localStorage.setItem("playerArray", JSON.stringify(playerArray));
+}
+
+function startQuiz() {
+  index = actualPlayer.answerAt;
+  quizNotify.setAttribute("style", "display:none");
+  formContainer.setAttribute("style", "display:visible");
+  quizButtons.setAttribute("style", "display:visible");
+
+  actualPlayer.finished = false;
+  if (storedAnswers.length > 0) {
+    //storedAnswers.splice(0, storedAnswers.length); //empty the array
+    storedAnswers.length = 0;  //empty the array
+  }
+  var stringQuestions = localStorage.getItem("allQuestions");
+  // Parse JSON string into object
+  allQuestions = JSON.parse(stringQuestions);
+  quizLength = allQuestions.length;
+
+  showQuestion();
+}
+
+function resumeQuiz() {
+  index = actualPlayer.answerAt;
+  quizNotify.setAttribute("style", "display:none");
+  formContainer.setAttribute("style", "display:visible");
+  quizButtons.setAttribute("style", "display:visible");
+
+  var stringQuestions = localStorage.getItem("allQuestions");
+  // Parse JSON string into object
+  allQuestions = JSON.parse(stringQuestions);
+  quizLength = allQuestions.length;
+  storedAnswers = actualPlayer.storedAnswers
+
+  showQuestion();
+}
+
+function showQuestion() {
+  if(index === 0) {
+    //there is no previous question when first question is shown
+    prevButton.setAttribute("style", "display:none");
+  }
+  if (index > 0) {
+    prevButton.style.display = "inline";
+  }
+  if(index === quizLength) {
+    //only if last question is shown user can see the score
+    scoreButton.setAttribute("style", "display:visible");
+    //scoreButton.style.display = "inline";
+    nextButton.setAttribute("style", "display:none");
+    //prevButton still visible so user can go back and change answers
+    var h2 = document.createElement("h2");
+    h2.innerHTML = "That's it! Would you like to see your score?";
+    form.appendChild(h2);
+    return;
+  }
+  else {
+    nextButton.setAttribute("style", "display:visible");
+    //nextButton.style.display = "inline";
+    scoreButton.setAttribute("style", "display:none");
+  }
+
+  // display of question at given index:
+  var quizItem = allQuestions[index];
+  var q = document.createElement("h3");
+  var text = document.createTextNode(quizItem.question);
+
+  q.appendChild(text);
+  form.appendChild(q);
+  var storedAnswer = storedAnswers[index];
+  var id;
+  //if question has been answered already
+  if (storedAnswer) {
+    id = storedAnswer.id;
+  }
+
+  // display of choices, belonging to the questions at given index:
+  choices = allQuestions[index].choices;
+
+  for (var i = 0; i < choices.length; i++) {
+    var p = document.createElement("p");
+
+    var input = document.createElement("input");
+    input.setAttribute("id", i);
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "question");
+
+    if (i === quizItem.correctAnswer) {
+      input.setAttribute("value", "1");
+    } else {
+      input.setAttribute("value", "0");
+    }
+
+    if (i == id) {
+      // if question is already answered, id has a value
+      input.setAttribute("checked", "checked");
+    }
+    //if radiobutton is clicked, the chosen answer is stored in a separate array: storedAnswers
+    input.addEventListener("click", storeAnswer);
+
+    var label = document.createElement("label");
+    var choice = document.createTextNode(choices[i]);
+    label.appendChild(choice);
+
+    p.appendChild(input);
+    p.appendChild(label);
+    form.appendChild(p);
+  }
+}
+
+
+// http://jsfiddle.net/hvG7k/
+function previousQuestion() {
+  //shows previous question, with chosen answer checked
+  index--;
+  form.innerHTML = "";
+  $("#form1").fadeOut(0, function() {
+    //index--;
+    var show = showQuestion();
+    $(this).attr('innerHTML', 'show').fadeIn(300);
+  });
+}
+
+function nextQuestion() {
+
+  //shows next question only if answer has been chosen
+  if (storedAnswers[index] == null) {
+    warning.innerHTML = "Please choose an answer!";
+    return;
+  }
+  index++;
+    warning.innerHTML = "";
+    form.innerHTML = "";
+    $("#form1").fadeOut(0, function() {
+      var show = showQuestion();
+      $(this).attr('innerHTML', 'show').fadeIn(300);
+    });
+
 }
 
 function storeAnswer(e) {
@@ -156,168 +372,40 @@ function storeAnswer(e) {
     storedAnswers[index] = answer;
 }
 
-// thanks to https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
-// this function loads the quiz questions from external JSON file
-function loadJSON(callback) {
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'http://localhost/quiz-web-app/js/quizQuestions.json', true);
-  //xobj.open('GET', 'file:///C:/Users/Marian standaard/Projecten/quiz-web-app/js/quizQuestions.json', true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState === 4 && xobj.status === 200) {
-      /* Required use of an anonymous callback as .open will NOT return a value
-      but simply returns undefined in asynchronous mode  */
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
+
+function showScore() {
+  form.innerHTML = "";
+  prevButton.setAttribute("style", "display:none");
+  scoreButton.setAttribute("style", "display:none");
+
+  quizNotify.setAttribute("style", "display:visible");
+  startButton.setAttribute("style", "display:visible");
+  resumeButton.setAttribute("style", "display:none");
+
+  var totalScore = 0;
+  actualPlayer.finished = true;
+
+  for (var i = 0; i < storedAnswers.length; i++) {
+    var score = parseInt(storedAnswers[i].value);
+    totalScore += score;
+  }
+
+
+  if (totalScore === allQuestions.length) {
+    message.innerHTML = "Great! Your score is " + totalScore + "!<br />You can do the quiz again, although you don't really need to!";
+  }
+  else if (totalScore <= 1) {
+    message.innerHTML = "You could use a litte practice! Your score is " + totalScore + ".<br />Would you like to do the quiz again?";
+  }
+  else {
+    message.innerHTML = "Well that's not too bad! Your score is " + totalScore + ".<br />Would you like to do the quiz again?";
+  }
+  actualPlayer.score = totalScore;
+  actualPlayer.storedScores.push(totalScore);
 }
 
-function init() {
-  loadJSON(function(response) {
-    // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
-    //quiz questions have been loaded
-    var quizLength = actual_JSON.length;
-
-    function showQuestion() {
-      var quizLength = actual_JSON.length;
-
-      // display of buttons:
-
-      if(index === 0) {
-        //there is no previous question when first question is shown
-        prevButton.style.display = "none";
-      }
-      if (index > 0) {
-        prevButton.style.display = "inline";
-      }
-      if(index === quizLength) {
-        //only if last question is shown user can see the score
-        scoreButton.style.display = "inline";
-        nextButton.style.display = "none";
-        //prevButton still visible so user can go back and change answers
-        var h2 = document.createElement("h2");
-        h2.innerHTML = "That's it! Would you like to see your score?";
-        form.appendChild(h2);
-        return;
-      }
-      else {
-        nextButton.style.display = "inline";
-        scoreButton.style.display = "none";
-      }
-
-      // display of question at given index:
-
-      var choices = actual_JSON[index].choices;
-      var storedAnswer = storedAnswers[index];
-      var id;
-      var quizItem = actual_JSON[index];
-      var q = document.createElement("h3");
-      var text = document.createTextNode(quizItem.question);
-
-      q.appendChild(text);
-      form.appendChild(q);
-      //if question has been answered already
-      if (storedAnswer) {
-        id = storedAnswer.id;
-      }
-
-      for (var i = 0; i < choices.length; i++) {
-        var p = document.createElement("p");
-
-        var input = document.createElement("input");
-        input.setAttribute("id", i);
-        input.setAttribute("type", "radio");
-        input.setAttribute("name", "question");
-
-        if (i === quizItem.correctAnswer) {
-          input.setAttribute("value", "1");
-        } else {
-          input.setAttribute("value", "0");
-        }
-
-        if (i == id) {
-          // if question is already answered, id has a value
-          input.setAttribute("checked", "checked");
-        }
-        //chosen answer is stored in a separate array: storedAnswers
-        input.addEventListener("click", storeAnswer);
-
-        var label = document.createElement("label");
-        var choice = document.createTextNode(choices[i]);
-        label.appendChild(choice);
-
-        p.appendChild(input);
-        p.appendChild(label);
-        form.appendChild(p);
-      }
-    }
-
-    showQuestion();
-
-    // http://jsfiddle.net/hvG7k/
-    function previousQuestion() {
-      //shows previous question, with chosen answer checked
-      welcome.innerHTML = "";
-      form.innerHTML = "";
-      $("#form1").fadeOut(0, function() {
-        index--;
-        var show = showQuestion();
-        $(this).attr('innerHTML', 'show').fadeIn(300);
-      });
-    }
-
-    function nextQuestion() {
-      //shows next question only if answer has been chosen
-      if (storedAnswers[index] === null) {
-        warning.innerHTML = "Please choose an answer!";
-        return;
-      }
-      else  {
-        warning.innerHTML = "";
-        welcome.innerHTML = "";
-        form.innerHTML = "";
-        $("#form1").fadeOut(0, function() {
-          index++;
-          var show = showQuestion();
-          $(this).attr('innerHTML', 'show').fadeIn(300);
-        });
-      }
-    }
-
-    function showScore() {
-      form.innerHTML = "";
-      prevButton.style.display = "none";
-      scoreButton.style.display = "none";
-      var totalScore = 0;
-
-      for (var i = 0; i < storedAnswers.length; i++) {
-        var score = parseInt(storedAnswers[i].value);
-        totalScore += score;
-      }
-
-      var h2 = document.createElement("h2");
-      form.appendChild(h2);
-
-      if (totalScore === actual_JSON.length) {
-        h2.innerHTML = "Great! Your score is " + totalScore + "!";
-      }
-      else if (totalScore <= 1) {
-        h2.innerHTML = "You could use a litte practice! Your score is " + totalScore + ".";
-      }
-      else {
-        h2.innerHTML = "Well that's not too bad! Your score is " + totalScore + ".";
-      }
-      actualPlayer.score = totalScore;
-      actualPlayer.storedScores.push(totalScore);
-      //setScore(user, totalScore);
-    }
-
-    prevButton.addEventListener("click", previousQuestion);
-    nextButton.addEventListener("click", nextQuestion);
-    scoreButton.addEventListener("click", showScore);
-  });
-}
+prevButton.addEventListener("click", previousQuestion);
+nextButton.addEventListener("click", nextQuestion);
+scoreButton.addEventListener("click", showScore);
 
 init();
